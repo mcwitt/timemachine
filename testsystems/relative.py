10c9ab565cfee24036333fe3b550b279d3b01c38
Yutong Zhao <proteneer@gmail.com>
2021-04-04 13:04:17 -0400

    setup identity

--- testsystems/relative.py
+++ testsystems/relative.py
@@ -30,39 +30,44 @@ def _setup_hif2a_ligand_pair(ff='ff/params/smirnoff_1_1_0_ccc.py'):
     suppl = Chem.SDMolSupplier(path_to_ligand, removeHs=False)
     all_mols = [x for x in suppl]
     mol_a = all_mols[1]
-    mol_b = all_mols[4]
+    mol_b = all_mols[1]
 
-    core = np.array([[0, 0],
-                     [2, 2],
-                     [1, 1],
-                     [6, 6],
-                     [5, 5],
-                     [4, 4],
-                     [3, 3],
-                     [15, 16],
-                     [16, 17],
-                     [17, 18],
-                     [18, 19],
-                     [19, 20],
-                     [20, 21],
-                     [32, 30],
-                     [26, 25],
-                     [27, 26],
-                     [7, 7],
-                     [8, 8],
-                     [9, 9],
-                     [10, 10],
-                     [29, 11],
-                     [11, 12],
-                     [12, 13],
-                     [14, 15],
-                     [31, 29],
-                     [13, 14],
-                     [23, 24],
-                     [30, 28],
-                     [28, 27],
-                     [21, 22]]
-                    , dtype=np.int32)
+    core = np.stack([
+        np.arange(mol_a.GetNumAtoms()),
+        np.arange(mol_a.GetNumAtoms())
+    ], axis=1).astype(np.int32)
+
+    # core = np.array([[0, 0],
+    #                  [2, 2],
+    #                  [1, 1],
+    #                  [6, 6],
+    #                  [5, 5],
+    #                  [4, 4],
+    #                  [3, 3],
+    #                  [15, 16],
+    #                  [16, 17],
+    #                  [17, 18],
+    #                  [18, 19],
+    #                  [19, 20],
+    #                  [20, 21],
+    #                  [32, 30],
+    #                  [26, 25],
+    #                  [27, 26],
+    #                  [7, 7],
+    #                  [8, 8],
+    #                  [9, 9],
+    #                  [10, 10],
+    #                  [29, 11],
+    #                  [11, 12],
+    #                  [12, 13],
+    #                  [14, 15],
+    #                  [31, 29],
+    #                  [13, 14],
+    #                  [23, 24],
+    #                  [30, 28],
+    #                  [28, 27],
+    #                  [21, 22]]
+    #                 , dtype=np.int32)
 
     single_topology = topology.SingleTopology(mol_a, mol_b, core, forcefield)
     rfe = free_energy.RelativeFreeEnergy(single_topology)
