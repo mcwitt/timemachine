8a74ecce512155f768666d61a010c9a4f73bf97e
Yutong Zhao <proteneer@gmail.com>
2021-04-17 01:42:09 -0400

    restore charges

--- fe/topology.py
+++ fe/topology.py
@@ -224,8 +224,8 @@ class BaseTopology():
 
         if stage == 'complex0' or stage == 'solvent':
             # REMOVE ME
-            src_qlj_params = jax.ops.index_update(qlj_params, jax.ops.index[:, 0], 0)
-            # src_qlj_params = qlj_params
+            # src_qlj_params = jax.ops.index_update(qlj_params, jax.ops.index[:, 0], 0)
+            src_qlj_params = qlj_params
             dst_qlj_params = jax.ops.index_update(qlj_params, jax.ops.index[:, 0], 0)
             dst_qlj_params = jax.ops.index_update(qlj_params, jax.ops.index[:, 1], safe_sigmas)
             dst_qlj_params = jax.ops.index_update(qlj_params, jax.ops.index[:, 2], safe_epsilons)
