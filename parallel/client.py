6b86866bb3789e50fe94031403b346cb578a3233
Yutong Zhao <proteneer@gmail.com>
2021-04-27 21:25:31 -0400

    WIP

--- parallel/client.py
+++ parallel/client.py
@@ -118,7 +118,7 @@ class CUDAPoolClient(ProcessPoolClient):
     @staticmethod
     def wrapper(idx, fn, *args):
         print("SETTING CUDA VISIBLE DEVICES TO", str(idx))
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(idx)
+        # os.environ['CUDA_VISIBLE_DEVICES'] = str(idx)
         return fn(*args)
 
     def submit(self, task_fn, *args):
