329f382e89da9b5a3a95feea58557e6d5519ef97
Josh Fass <jfass@relaytx.com>
2021-05-28 12:08:47 -0700

    Add optimized lambda schedule for water deletion moves

--- md/noneq/utils.py
+++ md/noneq/utils.py
@@ -47,3 +47,48 @@ def alchemify(potential_energy: PotentialEnergyModel, particle_indices: np.array
         **original_potential_energy_params
     )
     return alchemical_model
+
+
+def construct_water_deletion_lambda_schedule(num_windows=60):
+    """Interpolate the results from a previous schedule optimization
+
+    Notes
+    -----
+    * Adapted for deleting / inserting a single water in about ~10,000 MD steps
+    * Reversing this gives an approximately good lambda schedule for water insertion
+
+    TODO: rather than linearly interpolating raw data points this way, just precompute a few chebyshev coefficients
+    TODO: optimize with more controllable dials
+
+    See Also
+    --------
+    * fe.free_energy.construct_lambda_schedule -- manually optimized for relative calculations
+    """
+
+    # from applying https://github.com/proteneer/timemachine/blob/04d20878d1552efacac393171d39bb95f97430bf/scripts/noneq_adaptation/adapt_noneq.py#L98-L144
+    yp = np.array([0., 0.01533059, 0.02229805, 0.02690808, 0.03101898,
+                   0.03441388, 0.03791426, 0.04162575, 0.04488577, 0.04820164,
+                   0.05065692, 0.05316312, 0.05590272, 0.05873761, 0.06141923,
+                   0.06400197, 0.06637154, 0.06849308, 0.07079436, 0.07307307,
+                   0.07536735, 0.07751866, 0.08011125, 0.08231346, 0.08437469,
+                   0.08669951, 0.0889812, 0.09112025, 0.09335162, 0.09541216,
+                   0.09742787, 0.09929203, 0.10117189, 0.10370826, 0.10606132,
+                   0.10858983, 0.11071546, 0.11285919, 0.11467657, 0.11668331,
+                   0.11894838, 0.12109086, 0.12322202, 0.12590958, 0.12799611,
+                   0.12998178, 0.13209227, 0.13432605, 0.13611111, 0.13810413,
+                   0.14046818, 0.14231534, 0.14440701, 0.14660081, 0.14867329,
+                   0.15060446, 0.15302838, 0.15541909, 0.15768687, 0.16005014,
+                   0.16192792, 0.16405529, 0.16653765, 0.16813328, 0.17017239,
+                   0.17239472, 0.17520668, 0.17756246, 0.17940363, 0.1811484,
+                   0.18326328, 0.18558914, 0.18782285, 0.18990331, 0.19238311,
+                   0.19492217, 0.19782922, 0.20077991, 0.20373518, 0.20743844,
+                   0.21053796, 0.21356965, 0.21657056, 0.22061602, 0.22332947,
+                   0.22765496, 0.23361871, 0.23831932, 0.24281474, 0.24818963,
+                   0.25544663, 0.26228598, 0.27096489, 0.27995752, 0.29032133,
+                   0.3057806, 0.32154514, 0.33801365, 0.35913265, 0.38613041,
+                   0.40806747, 0.44087817, 0.47039889, 0.49554231, 0.52291761,
+                   1.])
+    xp = np.linspace(0, 1, len(yp))
+
+    x_interp = np.linspace(0, 1, num_windows)
+    return np.interp(x_interp, xp, yp)
