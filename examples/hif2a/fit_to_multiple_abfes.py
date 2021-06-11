21d3a0fc4f01d17ac06f8aed492c90c1b3fd4964
Yutong Zhao <proteneer@gmail.com>
2021-06-10 20:47:25 -0400

    Standardize solvent leg

--- examples/hif2a/fit_to_multiple_abfes.py
+++ examples/hif2a/fit_to_multiple_abfes.py
@@ -266,7 +266,7 @@ if __name__ == "__main__":
         dG_complex_conversion = binding_model_complex_conversion.predict(params, mol, prefix='complex_conversion_'+str(epoch))
         dG_complex_decouple = binding_model_complex_decouple.predict(params, mol, prefix='complex_decouple_'+str(epoch))
         dG_solvent_conversion = binding_model_solvent_conversion.predict(params, mol, prefix='solvent_conversion_'+str(epoch))
-        dG_solvent_decouple = binding_model_solvent_decouple.predict(params, mol, prefix='solvent_decouple'+str(epoch))
+        dG_solvent_decouple = binding_model_solvent_decouple.predict(params, mol, prefix='solvent_decouple'+str(epoch), standardize=True)
         print("complex dG_conversion", dG_complex_conversion)
         print("complex dG_decouple", dG_complex_decouple)
         print("solvent dG_conversion", dG_solvent_conversion)
