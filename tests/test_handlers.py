66b0d718990b708cc5c9c3622e0f32c3b73f36d1
Josh Fass <jfass@relaytx.com>
2021-08-04 14:34:30 -0400

    Extract assert_am1ccc_am1bcc_consistency from tests to ff/handlers/utils

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -6,12 +6,12 @@ import numpy as np
 
 import timemachine
 from pathlib import Path
-from ff import Forcefield
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from ff.handlers import nonbonded, bonded, AM1CCCHandler
+from ff.handlers import nonbonded, bonded
 from ff.handlers.deserialize import deserialize_handlers
+from ff.handlers.utils import assert_am1ccc_am1bcc_consistency
 
 import functools
 
@@ -377,46 +377,6 @@ $$$$
 
     # should be *exactly* identical since we're loading from cache
     np.testing.assert_array_equal(es_params_from_cache, es_params)
-
-
-def get_default_am1ccc_handler():
-    # load ff
-    root = Path(timemachine.__file__).parent.parent
-    path_to_ff = str(root.joinpath('ff/params/smirnoff_1_1_0_ccc.py'))
-
-    with open(path_to_ff) as f:
-        ff_handlers = deserialize_handlers(f.read())
-    ff = Forcefield(ff_handlers)
-
-    # get AM1CCC handler
-    ordered_handles = ff.get_ordered_handles()
-    components = [handle.__class__ for handle in ordered_handles]
-    handles = dict(zip(components, ordered_handles))
-    am1ccc_handler = handles[AM1CCCHandler]
-
-    return am1ccc_handler
-
-
-def assert_am1ccc_am1bcc_consistency(mols, abs_tolerance=1e-3):
-    """Assert that the partial charges assigned by ff/params/smirnoff_1_1_0_ccc.py
-    are close to those assigned by AM1BCCELF10, for all atoms in a collection of mols"""
-    # methods to compare
-    am1ccc_handler = get_default_am1ccc_handler()
-
-    def am1ccc_parameterize(mol):
-        return am1ccc_handler.parameterize(mol)
-
-    def am1bcc_parameterize(mol):
-        return nonbonded.oe_assign_charges(mol, 'AM1BCCELF10')
-
-    # run both methods on all mols
-    inlined_constant = np.sqrt(timemachine.constants.ONE_4PI_EPS0)
-
-    for mol in mols:
-        ref = am1bcc_parameterize(mol) / inlined_constant
-        test = am1ccc_parameterize(mol) / inlined_constant
-        difference = np.max(np.abs(ref - test))
-        assert difference < abs_tolerance, f'{difference:.3f} e > {abs_tolerance} e'
 
 
 def test_am1ccc_am1bcc_consistency_hif2a(max_checks=10, abs_tolerance=1e-3):
