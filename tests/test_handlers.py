ef6a2620f9c98da993b22edf2cab271a788303d3
Josh Fass <jfass@relaytx.com>
2021-08-04 12:18:23 -0400

    Add test_am1ccc_am1bcc_consistency

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -4,9 +4,13 @@ import jax
 import pytest
 import numpy as np
 
+import timemachine
+from pathlib import Path
+from ff import Forcefield
+
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from ff.handlers import nonbonded, bonded
+from ff.handlers import nonbonded, bonded, AM1CCCHandler
 from ff.handlers.deserialize import deserialize_handlers
 
 import functools
@@ -373,6 +377,57 @@ $$$$
 
     # should be *exactly* identical since we're loading from cache
     np.testing.assert_array_equal(es_params_from_cache, es_params)
+
+
+def get_default_am1ccc_handler():
+    # load ff
+    root = Path(timemachine.__file__).parent.parent
+    path_to_ff = str(root.joinpath('ff/params/smirnoff_1_1_0_ccc.py'))
+
+    with open(path_to_ff) as f:
+        ff_handlers = deserialize_handlers(f.read())
+    ff = Forcefield(ff_handlers)
+
+    # get AM1CCC handler
+    ordered_handles = ff.get_ordered_handles()
+    components = [handle.__class__ for handle in ordered_handles]
+    handles = dict(zip(components, ordered_handles))
+    am1ccc_handler = handles[AM1CCCHandler]
+
+    return am1ccc_handler
+
+
+def test_am1ccc_am1bcc_consistency(abs_tolerance=1e-3):
+    """Assert that the partial charges assigned by ff/params/smirnoff_1_1_0_ccc.py
+    to all atoms in all molecules in hif2a/ligands.sdf are close to the charges
+    assigned by AM1BCCELF10.
+
+    TODO: investigate why this does not pass on freesolv.sdf
+    """
+    # methods to compare
+    am1ccc_handler = get_default_am1ccc_handler()
+
+    def am1ccc_parameterize(mol):
+        return am1ccc_handler.parameterize(mol)
+
+    def am1bcc_parameterize(mol):
+        return nonbonded.oe_assign_charges(mol, 'AM1BCCELF10')
+
+    # data
+    root = Path(timemachine.__file__).parent.parent
+    path_to_hif2a_ligands = str(root.joinpath('datasets/fep-benchmark/hif2a/ligands.sdf'))
+    suppl = Chem.SDMolSupplier(path_to_hif2a_ligands, removeHs=False)
+    mols = [x for x in suppl]
+
+    # run both methods on all mols
+    inlined_constant = np.sqrt(timemachine.constants.ONE_4PI_EPS0)
+
+    for mol in mols:
+        ref = am1bcc_parameterize(mol) / inlined_constant
+        test = am1ccc_parameterize(mol) / inlined_constant
+        difference = np.max(np.abs(ref - test))
+        assert difference < abs_tolerance, f'{difference:.3f} e > {abs_tolerance} e'
+
 
 def test_simple_charge_handler():
 
