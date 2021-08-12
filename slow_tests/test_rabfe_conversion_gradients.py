7c3d51bbe8edc870ed1ca7792d713ec0dac35ec5
Josh Fass <jfass@relaytx.com>
2021-08-12 14:57:37 -0400

    Continue initializing restraints

--- slow_tests/test_rabfe_conversion_gradients.py
+++ slow_tests/test_rabfe_conversion_gradients.py
@@ -26,44 +26,64 @@ from timemachine.potentials import rmsd
 from md import builders, minimizer
 from rdkit.Chem import rdFMCS
 from fe.atom_mapping import CompareDistNonterminal
+from fe.utils import get_romol_conf
 
 with open('ff/params/smirnoff_1_1_0_ccc.py') as f:
     ff_handlers = deserialize_handlers(f.read())
 
 default_forcefield = Forcefield(ff_handlers)
+import timemachine
+from pathlib import Path
+
+root = Path(timemachine.__file__).parent.parent
+path_to_hif2a = root.joinpath('datasets/fep-benchmark/hif2a')
+
+def get_ligands(ligand_sdf):
+    suppl = Chem.SDMolSupplier(ligand_sdf, removeHs=False)
+    mols = [x for x in suppl]
+    return mols
 
 
 class SolventConversion():
-    def __init__(self, mol, mol_ref, solvent_system, solvent_topology,
+    def __init__(self, mol, mol_ref,
                  temperature=300, pressure=1.0, dt=2.5*1e-3,
                  num_equil_steps=100, num_prod_steps=1000,
-                 num_windows=10, client=CUDAPoolClient(1), forcefield=default_forcefield):
+                 num_windows=10, client=CUDAPoolClient(1),
+                 initial_forcefield=default_forcefield):
 
         self.mol = mol
         self.mol_ref = mol_ref
-
+        self.temperature = temperature
+        self.pressure = pressure
+        self.dt = dt
+        self.num_equil_steps = num_equil_steps
+        self.num_prod_steps = num_prod_steps
         self.schedule = construct_conversion_lambda_schedule(num_windows)
+        self.client = client
+        self.initial_forcefield = initial_forcefield
+        self.solvent_system, self.solvent_coords, self.solvent_box, self.solvent_topology = builders.build_water_system(
+            4.0)
 
-        conversion_model = model_rabfe.AbsoluteConversionModel(
-            client,
-            forcefield,
-            solvent_system,
+        self.conversion_model = model_rabfe.AbsoluteConversionModel(
+            self.client,
+            self.initial_forcefield,
+            self.solvent_system,
             self.schedule,
-            solvent_topology,
-            temperature,
-            pressure,
-            dt,
-            num_equil_steps,
-            num_prod_steps
+            self.solvent_topology,
+            self.temperature,
+            self.pressure,
+            self.dt,
+            self.num_equil_steps,
+            self.num_prod_steps
         )
-        self.conversion_model = conversion_model
+        self.initialize_restraints()
 
+    def initialize_restraints(self):
         mcs_params = rdFMCS.MCSParameters()
         mcs_params.AtomTyper = CompareDistNonterminal()
         mcs_params.BondTyper = rdFMCS.BondCompare.CompareAny
         self.mcs_params = mcs_params
 
-    def initialize_restraints(self, complex_coords, complex_ref_x0, complex_ref_box0):
         result = rdFMCS.FindMCS(
             [self.mol, self.mol_ref],
             self.mcs_params
@@ -77,19 +97,16 @@ class SolventConversion():
         core_idxs = setup_relative_restraints_using_smarts(self.mol, self.mol_ref, core_smarts)
         mol_coords = get_romol_conf(self.mol)  # original coords
 
-        num_complex_atoms = complex_coords.shape[0]
+        x_mol = get_romol_conf(self.mol)
+        x_mol_ref = get_romol_conf(self.mol_ref)
 
         # Use core_idxs to generate
         R, t = rmsd.get_optimal_rotation_and_translation(
-            x1=complex_ref_x0[num_complex_atoms:][core_idxs[:, 1]],  # reference core atoms
-            x2=mol_coords[core_idxs[:, 0]],  # mol core atoms
+            x1=x_mol_ref[core_idxs[:, 1]],  # reference core atoms
+            x2=x_mol[core_idxs[:, 0]],  # mol core atoms
         )
 
         aligned_mol_coords = rmsd.apply_rotation_and_translation(mol_coords, R, t)
-
-        ref_coords = complex_ref_x0[num_complex_atoms:]
-        complex_host_coords = complex_ref_x0[:num_complex_atoms]
-        complex_box0 = complex_ref_box0
 
         raise NotImplementedError
 
@@ -107,13 +124,28 @@ class SolventConversion():
         solvent_x0 = np.concatenate([min_solvent_coords, mol_coords])
         solvent_box0 = solvent_box
         dG_solvent_conversion, dG_solvent_conversion_error = binding_model_solvent_conversion.predict(
-            params,
+            ordered_params,
             mol,
             solvent_x0,
             solvent_box0,
             prefix='solvent_conversion_' + mol_name + "_" + str(epoch)
         )
 
+        return dG_solvent_conversion
+
 def test_rabfe_conversion_trainable():
+    """does not test for correctness, just that unconverged simulations"""
+
+    ligand_sdf = str(path_to_hif2a.joinpath('ligands.sdf').resolve())
+    mols = get_ligands(ligand_sdf)
+    mol, mol_ref = mols[:2]
+
+
+    solvent_conversion = SolventConversion(mol, mol_ref, solvent_system, solvent_topology)
+
+    flat_params = solvent_conversion.flatten(default_forcefield.get_ordered_params())
+
+    initial_prediction = solvent_conversion.predict()
+
 
     raise NotImplementedError
