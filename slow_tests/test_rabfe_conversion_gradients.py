04ed7bfc4321c1fc66bd7d09a0b31713ee91e788
Josh Fass <jfass@relaytx.com>
2021-08-12 19:48:18 -0400

    Test complex conversion
    Introduces substantial code duplication within test

--- slow_tests/test_rabfe_conversion_gradients.py
+++ slow_tests/test_rabfe_conversion_gradients.py
@@ -37,6 +37,7 @@ from pathlib import Path
 root = Path(timemachine.__file__).parent.parent
 path_to_hif2a = root.joinpath('datasets/fep-benchmark/hif2a')
 ligand_sdf = str(path_to_hif2a.joinpath('ligands.sdf').resolve())
+protein_pdb = str(path_to_hif2a.joinpath('5tbm_prepared.pdb').resolve())
 
 def get_ligands(ligand_sdf):
     print(f'loading ligands from {ligand_sdf}')
@@ -94,6 +95,57 @@ class SolventConversion():
 
         return dG_solvent_conversion
 
+
+class ComplexConversion():
+    def __init__(self, mol, mol_ref, protein_pdb,
+                 temperature=300, pressure=1.0, dt=2.5*1e-3,
+                 num_equil_steps=100, num_prod_steps=1001,
+                 num_windows=2, client=CUDAPoolClient(2),
+                 initial_forcefield=default_forcefield):
+
+        self.mol = mol
+        self.mol_ref = mol_ref
+        self.schedule = construct_conversion_lambda_schedule(num_windows)
+        self.initial_forcefield = initial_forcefield
+
+        self.complex_system, self.complex_coords, _, _, self.complex_box, self.complex_topology = \
+            builders.build_protein_system(protein_pdb)
+        self.conversion_model = model_rabfe.AbsoluteConversionModel(
+            client,
+            self.initial_forcefield,
+            self.complex_system,
+            self.schedule,
+            self.complex_topology,
+            temperature,
+            pressure,
+            dt,
+            num_equil_steps,
+            num_prod_steps
+        )
+
+        self.flatten, self.unflatten = flatten_and_unflatten(self.initial_forcefield.get_ordered_params())
+
+    def predict(self, flat_params):
+        ordered_params = self.unflatten(flat_params)
+        mol_coords = get_romol_conf(self.mol)
+
+        # TODO: double-check if this should use initial forcefield, or if I need
+        #  to reconstruct a params-dependent forcefield and pass it here...
+        forcefield_for_minimization = self.initial_forcefield
+        min_solvent_coords = minimizer.minimize_host_4d([self.mol], self.complex_system, self.complex_coords,
+                                                        forcefield_for_minimization, self.complex_box)
+        x0 = np.concatenate([min_solvent_coords, mol_coords])
+        box0 = self.complex_box
+        dG_complex_conversion, dG_complex_conversion_error = self.conversion_model.predict(
+            ordered_params,
+            self.mol,
+            x0,
+            box0,
+            prefix='complex_conversion_test'
+        )
+
+        return dG_complex_conversion
+
 def test_rabfe_solvent_conversion_trainable(n_steps=10):
     """test that the loss goes down"""
     mols = get_ligands(ligand_sdf)
@@ -110,6 +162,56 @@ def test_rabfe_solvent_conversion_trainable(n_steps=10):
 
     def loss(params):
         residual = solvent_conversion.predict(params) - label
+        return l1_loss(residual)
+
+    def step(x, v, g):
+        raw_search_direction = - g
+        search_direction = raw_search_direction * learning_rates
+
+        x_increment = truncated_step(x, v, g, search_direction=search_direction)
+        x_next = x + x_increment
+
+        return x_next
+
+    flat_param_traj = [initial_flat_params]
+    loss_traj = [l1_loss(initial_prediction - label)]
+    print(f'initial loss: {loss_traj[-1]:.3f}')
+
+    for t in range(n_steps):
+        x = flat_param_traj[-1]
+        v, g = value_and_grad(loss)(x)
+        x_next = step(x, v, g)
+
+        print(x_next - x)
+
+        print(f'epoch {t}: loss = {v:.3f}, gradient norm = {np.linalg.norm(g):.3f}')
+
+        flat_param_traj.append(x_next)
+        loss_traj.append(v)
+
+    window_size = min(5, n_steps // 2)
+    before = loss_traj[0]
+    after = np.median(loss_traj[-window_size:])
+
+    assert after < before, f"before: {before:.3f}, after: {after:.3f}"
+
+
+def test_rabfe_complex_conversion_trainable(n_steps=10):
+    """test that the loss goes down"""
+    mols = get_ligands(ligand_sdf)
+    mol, mol_ref = mols[:2]
+
+    complex_conversion = ComplexConversion(mol, mol_ref, protein_pdb)
+
+    initial_flat_params = complex_conversion.flatten(ordered_params)
+    learning_rates = complex_conversion.flatten(ordered_learning_rates)
+
+    initial_prediction = complex_conversion.predict(initial_flat_params)
+
+    label = initial_prediction - 100
+
+    def loss(params):
+        residual = complex_conversion.predict(params) - label
         return l1_loss(residual)
 
     def step(x, v, g):
