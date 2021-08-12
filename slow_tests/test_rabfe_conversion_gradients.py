2b098eadc51e4912f25706911b678ce1238b3c69
Josh Fass <jfass@relaytx.com>
2021-08-12 17:00:40 -0400

    Move print statement

--- slow_tests/test_rabfe_conversion_gradients.py
+++ slow_tests/test_rabfe_conversion_gradients.py
@@ -43,6 +43,7 @@ root = Path(timemachine.__file__).parent.parent
 path_to_hif2a = root.joinpath('datasets/fep-benchmark/hif2a')
 
 def get_ligands(ligand_sdf):
+    print(f'loading ligands from {ligand_sdf}')
     suppl = Chem.SDMolSupplier(ligand_sdf, removeHs=False)
     mols = [x for x in suppl]
     return mols
@@ -108,7 +109,6 @@ def test_rabfe_conversion_trainable(n_steps=100):
     """test that the loss goes down"""
 
     ligand_sdf = str(path_to_hif2a.joinpath('ligands.sdf').resolve())
-    print(f'loading ligands from {ligand_sdf}')
     mols = get_ligands(ligand_sdf)
     mol, mol_ref = mols[:2]
 
