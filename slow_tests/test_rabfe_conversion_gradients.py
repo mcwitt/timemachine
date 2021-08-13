6066dd15fea314972cdeb1ddfb78cbdf71a33a40
Josh Fass <jfass@relaytx.com>
2021-08-13 14:44:43 -0400

    Eliminate 2x code duplication
    Between test_rabfe_solvent_conversion_trainable and test_rabfe_complex_conversion_trainable

--- slow_tests/test_rabfe_conversion_gradients.py
+++ slow_tests/test_rabfe_conversion_gradients.py
@@ -26,98 +26,61 @@ ordered_learning_rates = learning_rates_like_params(ordered_handles, ordered_par
 flatten, unfllatten = flatten_and_unflatten(ordered_params)
 learning_rates = flatten(ordered_learning_rates)
 
+mols = get_ligands()
+mol, mol_ref = mols[:2]
 
-def test_rabfe_solvent_conversion_trainable(n_steps=10):
+
+def assert_conversion_trainable(conversion, n_epochs=10):
     """test that the loss goes down"""
-    mols = get_ligands()
-    mol, mol_ref = mols[:2]
 
+    initial_flat_params = flatten(ordered_params)
+    initial_prediction = conversion.predict(initial_flat_params)
+
+    label = initial_prediction - 100
+
+    def loss(params):
+        residual = conversion.predict(unfllatten(params)) - label
+        return l1_loss(residual)
+
+    def step(x, v, g):
+        raw_search_direction = - g
+        search_direction = raw_search_direction * learning_rates
+
+        x_increment = truncated_step(x, v, g, search_direction=search_direction)
+        x_next = x + x_increment
+
+        return x_next
+
+    flat_param_traj = [initial_flat_params]
+    loss_traj = [l1_loss(initial_prediction - label)]
+    print(f'initial loss: {loss_traj[-1]:.3f}')
+
+    for t in range(n_epochs):
+        x = flat_param_traj[-1]
+        v, g = value_and_grad(loss)(x)
+        x_next = step(x, v, g)
+
+        print(x_next - x)
+
+        print(f'epoch {t}: loss = {v:.3f}, gradient norm = {np.linalg.norm(g):.3f}')
+
+        flat_param_traj.append(x_next)
+        loss_traj.append(v)
+
+    window_size = min(5, n_epochs // 2)
+    before = loss_traj[0]
+    after = np.median(loss_traj[-window_size:])
+
+    assert after < before, f"before: {before:.3f}, after: {after:.3f}"
+
+
+def test_rabfe_solvent_conversion_trainable():
+    """test that the loss goes down"""
     solvent_conversion = SolventConversion(mol, mol_ref, default_forcefield)
-
-    initial_flat_params = flatten(ordered_params)
-    initial_prediction = solvent_conversion.predict(initial_flat_params)
-
-    label = initial_prediction - 100
-
-    def loss(params):
-        residual = solvent_conversion.predict(params) - label
-        return l1_loss(residual)
-
-    def step(x, v, g):
-        raw_search_direction = - g
-        search_direction = raw_search_direction * learning_rates
-
-        x_increment = truncated_step(x, v, g, search_direction=search_direction)
-        x_next = x + x_increment
-
-        return x_next
-
-    flat_param_traj = [initial_flat_params]
-    loss_traj = [l1_loss(initial_prediction - label)]
-    print(f'initial loss: {loss_traj[-1]:.3f}')
-
-    for t in range(n_steps):
-        x = flat_param_traj[-1]
-        v, g = value_and_grad(loss)(x)
-        x_next = step(x, v, g)
-
-        print(x_next - x)
-
-        print(f'epoch {t}: loss = {v:.3f}, gradient norm = {np.linalg.norm(g):.3f}')
-
-        flat_param_traj.append(x_next)
-        loss_traj.append(v)
-
-    window_size = min(5, n_steps // 2)
-    before = loss_traj[0]
-    after = np.median(loss_traj[-window_size:])
-
-    assert after < before, f"before: {before:.3f}, after: {after:.3f}"
+    assert_conversion_trainable(solvent_conversion, 10)
 
 
-def test_rabfe_complex_conversion_trainable(n_steps=10):
+def test_rabfe_complex_conversion_trainable():
     """test that the loss goes down"""
-    mols = get_ligands()
-    mol, mol_ref = mols[:2]
-
     complex_conversion = ComplexConversion(mol, mol_ref, protein_pdb, default_forcefield)
-
-    initial_flat_params = flatten(ordered_params)
-    initial_prediction = complex_conversion.predict(initial_flat_params)
-
-    label = initial_prediction - 100
-
-    def loss(params):
-        residual = complex_conversion.predict(params) - label
-        return l1_loss(residual)
-
-    def step(x, v, g):
-        raw_search_direction = - g
-        search_direction = raw_search_direction * learning_rates
-
-        x_increment = truncated_step(x, v, g, search_direction=search_direction)
-        x_next = x + x_increment
-
-        return x_next
-
-    flat_param_traj = [initial_flat_params]
-    loss_traj = [l1_loss(initial_prediction - label)]
-    print(f'initial loss: {loss_traj[-1]:.3f}')
-
-    for t in range(n_steps):
-        x = flat_param_traj[-1]
-        v, g = value_and_grad(loss)(x)
-        x_next = step(x, v, g)
-
-        print(x_next - x)
-
-        print(f'epoch {t}: loss = {v:.3f}, gradient norm = {np.linalg.norm(g):.3f}')
-
-        flat_param_traj.append(x_next)
-        loss_traj.append(v)
-
-    window_size = min(5, n_steps // 2)
-    before = loss_traj[0]
-    after = np.median(loss_traj[-window_size:])
-
-    assert after < before, f"before: {before:.3f}, after: {after:.3f}"
+    assert_conversion_trainable(complex_conversion, 10)
